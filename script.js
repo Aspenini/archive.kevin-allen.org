a4f4c53f5785686974adc2aa46de1dceb836583d
Aspen Feltner <bobfeltner123@gmail.com>
2025-04-28 22:54:24 -0400

    add media

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
+
 function openModal(imgElement) {
-    const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modal-img');
-    modal.style.display = "flex";
-    modalImg.src = imgElement.src;
+  const modal = document.getElementById('modal');
+  const modalImg = document.getElementById('modal-img');
+  modal.style.display = "flex";
+  modalImg.src = imgElement.src;
+}
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = "none";
+}
+function showCategory(category) {
+  const categories = document.getElementsByClassName('category');
+  for (let i = 0; i < categories.length; i++) {
+    categories[i].style.display = 'none';
   }
-  
-  function closeModal() {
-    const modal = document.getElementById('modal');
-    modal.style.display = "none";
-  }
-  
-  function showCategory(category) {
-    const categories = document.getElementsByClassName('category');
-    for (let i = 0; i < categories.length; i++) {
-      categories[i].style.display = 'none';
-    }
-    document.getElementById(category).style.display = 'flex';
-  }
-  
+  document.getElementById(category).style.display = 'flex';
+}
